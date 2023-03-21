4610dfb2c19b3bff41ff4ca8dd461215ee8bcb85
julianewaied <116627403+julianewaied@users.noreply.github.com>
2023-03-21 02:30:47 +0200

    function definition

--- HW1/readTrainData.py
+++ HW1/readTrainData.py
@@ -1,4 +1,6 @@
-
+import numpy as np
+import seaborn as sbs
+labels = []
 def readTrainData(file_name):
     file = open(file_name, 'r')
     lines = file.readlines()
@@ -7,7 +9,9 @@ def readTrainData(file_name):
     voc = []
     for line in lines:
         splitted = line.split('\t')
+        # this is all labels
         lbAll.append(splitted[0])
+        # this is all words 
         texAll.append(splitted[1].split())
         words = splitted[1].split()
         for w in words:
@@ -15,4 +19,18 @@ def readTrainData(file_name):
     voc = set(voc)
     cat = set(lbAll)
     return texAll, lbAll, voc, cat
+def find_class_conditional(word, label, voc):
+    # returns an array of class conditional (Pw), should use laplace smoothing
+    return 0
+def find_prior(label,voc):
+    # returns the prior of a label (P)
+    return 0
+def classify(sentence,Pw,P):
+    # use MAP and find the most probable class 
+    return 0
+def classify_NB_test(Pw,P):
+    return 0
+
+print('hello world')
+
 
