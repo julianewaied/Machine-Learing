2e8bd3e168d0f91b1dfa9a054ca89fb72c9740ed
julianewaied <116627403+julianewaied@users.noreply.github.com>
2023-03-24 12:19:47 +0300

    got 85% accuracy

--- HW1/readTrainData.py
+++ HW1/readTrainData.py
@@ -19,6 +19,12 @@ def readTrainData(file_name):
     voc = set(voc)
     cat = set(lbAll)
     return texAll, lbAll, voc, cat
+def count_label(lbAll,texAll,label):
+    count = 0
+    for i in range(0,len(lbAll)):
+        if(lbAll[i]==label):
+            count = count + len(texAll[i])
+    return count
 def find_class_conditional(texAll,lbAll,voc,cat):
     # returns a nested dictionary of class conditional (Pw), should use laplace smoothing
     # class conditionals: P(word | label)
@@ -33,11 +39,12 @@ def find_class_conditional(texAll,lbAll,voc,cat):
     for i in range(0,len(texAll)):
         for word in texAll[i]:
             class_conditionals[lbAll[i]][word] = class_conditionals[lbAll[i]][word] + 1
+                
     # applying laplace smoothing
     for label in cat:
-        count = lbAll.count(label) + 2
+        count = count_label(lbAll,texAll,label)
         for word in voc:
-            class_conditionals[label][word] = log2((class_conditionals[label][word] + 1) / count)
+            class_conditionals[label][word] = log2(class_conditionals[label][word] / count)
     return class_conditionals
 def find_prior(cat,lbAll):
     # returns the prior of a label (P)
@@ -49,17 +56,16 @@ def find_prior(cat,lbAll):
         count = lbAll.count(label)
         prior = count/len(lbAll)
         priors[label] = log2(prior)
-        counts[label] = -log2((count +2))
+        counts[label] = log2(1/(count +2))
     return priors, counts
 def calc_posterior(sentence,label,Pw,P,counts,voc):
     p = P[label]
     for word in sentence:
         # summing log2 of likelihood times prior
-        if word in voc:
+        if (word in voc) and (Pw[label][word] != 0):
             p = p + Pw[label][word]
         else:
             p = p + counts[label]
-    print(sentence,label,2**p)
     return p
 def classify(sentence,Pw,P,cat,counts,voc):
     # use MAP and find the most probable class 
@@ -74,14 +80,13 @@ def classify(sentence,Pw,P,cat,counts,voc):
 def classify_NB_test(Pw,P,cat,counts):
     return 0
 
-print('hello world')
-texAll, lbAll, voc, cat = readTrainData("train-test.txt")
+texAll, lbAll, voc, cat = readTrainData("r8-train-stemmed.txt")
 Pw = find_class_conditional(texAll,lbAll,voc,cat)
 P,counts = find_prior(cat,lbAll)
-error = 0
-texAll1, lbAll1, voc1, cat1 = readTrainData("test-test.txt")
+success = 0
+texAll1, lbAll1, voc1, cat1 = readTrainData("r8-test-stemmed.txt")
 for i in range(0,len(texAll1)):
     label = classify(texAll1[i],Pw,P,cat,counts,voc)
-    if(label != lbAll1[i]):
-        error+=1
-print('error rate is : ', error/len(texAll1))
+    if(label == lbAll1[i]):
+        success+=1
+print('sucess rate is  : ', success/len(texAll1))
