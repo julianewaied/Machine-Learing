c24ab149f5c0b6ebfb8394d187e5aafdb400a14a
julianewaied <116627403+julianewaied@users.noreply.github.com>
2023-05-11 00:40:04 +0300

    logistic done

--- HW2/q3/q3.py
+++ HW2/q3/q3.py
@@ -1,6 +1,9 @@
 import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
+from time import sleep
 
+data_path = "C:/Users/WIN10PRO/Desktop/My Stuff/University/BSC/Machine Learning/Machine-Learing/HW2/q3/exams.csv"
 def plot(data, labels, w):
 	fig, ax = plt.subplots()
 
@@ -20,12 +23,82 @@ def plot(data, labels, w):
 
 	plt.show()
 
+def test_model(model,data):
+	samples, labels = preprocess(data)
+	count = 0
+	for i in range(0,len(samples)):
+		if(model.classify(samples[i])==labels[i]):
+			count += 1
+	return count/float(len(samples))
+		
+def load(path):
+	data = pd.read_csv(path,header=None)
+	train = data.head(int(len(data)*0.9)).to_numpy()
+	test = data.tail(len(data)-len(train)).to_numpy()
+	return train,test
+
+def preprocess(data,change=True):
+	points = np.array([np.array([x[0],x[1],1]) for x in data])
+	labels = np.array([x[2] for x in data])
+	if(change):
+		labels[labels == 0] = -1
+	return points,labels
+
 class logistic_regression:
-	def __init__(self):
-		w=[]
-# accuracy = 0
-# print(f"Avg test accuracy: {accuracy * 100}%")	
+	def __init__(self,learning_rate = 1,convergance_constant = 0.1):
+		self.etta = learning_rate
+		self.convergance = convergance_constant
+	def calc_grad(self,samples,lbl):
+		vec_grad = np.vectorize(self.single_grad, signature='(n),()->(n)')
+		grads = vec_grad(samples,lbl)
+		gradient = np.sum(grads,axis = 0)
+		return gradient
+	def single_grad(self, x,y):
+		# to prevent overflow
+		g = self.g(x)
+		g = np.clip(g, -500, 500)
+		scalar = (-y)/(1+np.exp(y*g))
+		answer = scalar * x
+		return answer
+	def train(self,data):
+		# iterate with gradient descent
+		# w is initially 0 vector
+		samples, labels = preprocess(data)
+		self.w = np.array([0 for i in range(0,len(samples[0]))])
+		i=0
+		prev = 0
+		curr =np.linalg.norm(self.calc_grad(samples,labels))
+		while(curr>self.convergance):
+			gradient = self.calc_grad(samples,labels)
+			norm = np.linalg.norm(gradient)
+			if(norm<curr):
+				if(norm<0.98*curr and self.etta>0.0001):
+					self.etta = self.etta/2
+				prev = curr
+				curr = norm
+				print(f'i = {i}, grad = {(curr)} etta= {self.etta}')
+			self.w = self.w - self.etta * gradient
+			i+=1
+				
+		return self.w
+	def classify(self,x):
+		return np.sign(self.g(x))
+	def g(self,x):
+		# w includes w0 - wd
+		return np.dot(self.w,x)
+	def setW(self,w):
+		self.w = w
 
 
 
-#-----------------------------------------
+train,test = load(data_path)
+model = logistic_regression(convergance_constant=0.357,learning_rate=2)
+d,l = preprocess(train,change = False)
+print('training...')
+w = model.train(train)
+# print('w = ', w)
+train_acc =test_model(model,train)*100
+print('train acc : ',train_acc,'%')
+plot(d,l,w)
+accuracy = test_model(model,test)
+print(f"Avg test accuracy: {accuracy * 100}%")	
---
+++ HW2/q3/test.py
@@ -0,0 +1,42 @@
+﻿import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.linear_model import LogisticRegression
+
+# Read the CSV file into a pandas dataframe
+df = pd.read_csv('C:/Users/WIN10PRO/Desktop/My Stuff/University/BSC/Machine Learning/Machine-Learing/HW2/q3/exams.csv', header=None)
+
+# Split the dataframe into training and testing sets
+train = df.iloc[:90, :]
+test = df.iloc[90:, :]
+
+# Split the training and testing sets into features (X) and labels (y)
+X_train = train.iloc[:, :-1]
+y_train = train.iloc[:, -1]
+X_test = test.iloc[:, :-1]
+y_test = test.iloc[:, -1]
+
+# Create a logistic regression object and fit it to the training data
+lr = LogisticRegression()
+lr.fit(X_train, y_train)
+
+# Print the coefficients of the logistic regression
+w = lr.coef_[0]
+print('Vector w:', w)
+
+# Calculate the accuracy of the model on the training and testing data
+y_train_pred = lr.predict(X_train)
+accuracy_train = np.mean(y_train_pred == y_train)
+print('Training accuracy:', accuracy_train)
+
+y_test_pred = lr.predict(X_test)
+accuracy_test = np.mean(y_test_pred == y_test)
+print('Testing accuracy:', accuracy_test)
+
+# Plot the points and the LDF
+plt.scatter(train.iloc[:, 0], train.iloc[:, 1], c=train.iloc[:, -1].map({0: 'red', 1: 'blue'}), edgecolors='k')
+plt.scatter(test.iloc[:, 0], test.iloc[:, 1], c=test.iloc[:, -1].map({0: 'red', 1: 'blue'}), s=40, edgecolors='k')
+x_vals = np.array(plt.gca().get_xlim())
+y_vals = -(w[0]*x_vals + lr.intercept_)/w[1]
+plt.plot(x_vals, y_vals, '--')
+plt.show()
